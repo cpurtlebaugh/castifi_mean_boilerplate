78d596dca1a9012260087f06ee87c99c12448c91
Greg Rock <grock006@gmail.com>
2015-10-14 16:01:26 -0700

    removed karma conf files not needed

--- client/app/actor/actor.controller.spec.js
+++ client/app/actor/actor.controller.spec.js
@@ -15,7 +15,7 @@ describe('Controller: ActorCtrl', function () {
     });
   }));
 
-  it('should ...', function () {
-    expect(1).toEqual(1);
-  });
+  // it('should ...', function () {
+  //   expect(1).toEqual(1);
+  // });
 });
