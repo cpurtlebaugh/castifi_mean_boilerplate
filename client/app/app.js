29d8d86f23b75fed7a2061629e10e77a44824e1c
Greg Rock <grock006@gmail.com>
2015-10-20 12:11:25 -0700

    added bower components ngImgCrop

--- client/app/app.js
+++ client/app/app.js
@@ -7,7 +7,8 @@ angular.module('castifiApp', [
   'btford.socket-io',
   'ui.router',
   'ui.bootstrap',
-  'ngFileUpload'
+  'ngFileUpload',
+  'ngImgCrop'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
