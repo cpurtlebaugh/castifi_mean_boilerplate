6acc404fb3667eda0910aed6ef67093b3f8c20cc
Greg Rock <grock006@gmail.com>
2015-10-29 11:00:43 -0700

    set production js back to original angular fullstack settings for heroku deployment

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -10,17 +10,17 @@ module.exports = {
 
   // Server port
   port:
-            // process.env.OPENSHIFT_NODEJS_PORT ||
-            // process.env.PORT ||
+            process.env.OPENSHIFT_NODEJS_PORT ||
+            process.env.PORT ||
             8080,
 
   // MongoDB connection options
   mongo: {
     uri:
-            // process.env.MONGOLAB_URI ||
-            // process.env.MONGOHQ_URL ||
-            // process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            // 'mongodb://localhost/castifi' ||
+            process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            'mongodb://localhost/castifi' ||
             'mongodb://castifiReadWrite:castifiReadWrite@ec2-52-8-131-20.us-west-1.compute.amazonaws.com:27017/castifiProdOne'
   }
 
