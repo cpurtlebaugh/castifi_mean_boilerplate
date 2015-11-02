30bd30d0a2bee0d1f0a208a97ed2ba8c1e304f20
Greg Rock <grock006@gmail.com>
2015-11-02 15:18:52 -0800

    added confirm password field, altered styling for tablet view screen

--- client/app/app.js
+++ client/app/app.js
@@ -8,7 +8,8 @@ angular.module('castifiApp', [
   'ui.router',
   'ui.bootstrap',
   'ngFileUpload',
-  'ngImgCrop'
+  'ngImgCrop',
+  'validation.match'
 ])
   .config(function ($stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
     $urlRouterProvider
