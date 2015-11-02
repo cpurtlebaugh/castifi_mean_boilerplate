934dd239b4ddce10fa3b08e3bc72ab8fb30ae8f7
Greg Rock <grock006@gmail.com>
2015-11-02 15:27:18 -0800

    modifified production js back to heroku settings

--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -10,18 +10,18 @@ module.exports = {
 
   // Server port
   port:
-            // process.env.OPENSHIFT_NODEJS_PORT ||
-            // process.env.PORT ||
+            process.env.OPENSHIFT_NODEJS_PORT ||
+            process.env.PORT ||
             8080,
 
   // MongoDB connection options
   mongo: {
     uri:
-            // process.env.MONGOLAB_URI ||
-            // process.env.MONGOHQ_URL ||
-            // process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
-            // 'mongodb://localhost/castifi' ||
-            'mongodb://castifiReadWrite:castifiReadWrite@ec2-52-8-131-20.us-west-1.compute.amazonaws.com:27017/castifiProdOne'
+            process.env.MONGOLAB_URI ||
+            process.env.MONGOHQ_URL ||
+            process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
+            'mongodb://localhost/castifi' ||
+            // 'mongodb://castifiReadWrite:castifiReadWrite@ec2-52-8-131-20.us-west-1.compute.amazonaws.com:27017/castifiProdOne'
   }
 
 };
