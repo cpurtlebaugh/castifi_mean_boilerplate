620216bc809cf24cccedd1654cf7869ab1f037f8
Cassie Purtlebaugh <cassie.purtlebaugh@gmail.com>
2015-11-03 15:58:20 -0800

    Add account.scss move login.scss to account.scss Update settings.html and controller to change email as well

--- client/app/account/settings/settings.controller.js
+++ client/app/account/settings/settings.controller.js
@@ -7,56 +7,60 @@ angular.module('castifiApp')
     $scope.user.newEmail = "";
     console.log($scope.user)
 
-    $scope.changeEmail = function(form) {
+    $scope.changeEmail = function(form, $event) {
       console.log("change email working")
       console.log(form)
       console.log($scope.user._id)
       console.log($scope.user.newEmail)
       console.log(form.$valid)
+      $event.stopPropagation();
+      $event.preventDefault();
+      console.log($event)
       $scope.submitted = true;
-      
+
       if(form.$valid) {
         console.log($scope.user.newEmail)
-        User.update({id: $scope.user._id}, {email: $scope.user.newEmail}, 
+        User.update({id: $scope.user._id}, {email: $scope.user.newEmail},
           function success(data){
              console.log(data)
              $scope.emailMessage = 'Email successfully changed.';
-          }),
+             $scope.user.oldEmail = "";
+             $scope.user.newEmail = "";
+          },
           function error(data){
             console.log(data)
-          }
-        // .then( function(data) {
-        //   console.log(data)
-        //   $scope.message = 'Password successfully changed.';
-      
-        // })
-        // .catch( function() {
-        //   form.password.$setValidity('mongoose', false);
-        //   $scope.errors.other = 'Incorrect password';
-        //   $scope.message = '';
-        // });
-      }
-    };
-  
+          })
+      } else {
+        console.log('i am in the else statement')
+          form.newEmail.$setValidity('mongoose', false);
+          $scope.errors.other = 'Please provide a valid email format.';
+          $scope.message = '';
+        }
 
-    $scope.changePassword = function(form) {
+    };
+
+
+    $scope.changePassword = function(form, $event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+
       $scope.submitted = true;
       if(form.$valid) {
         Auth.changePassword( $scope.user.oldPassword, $scope.user.newPassword )
         .then( function() {
           $scope.message = 'Password successfully changed.';
-        
+
         })
         .catch( function() {
-      
-          form.password.$setValidity('mongoose', false);
+
+          form.passwordForm.$setValidity('mongoose', false);
           $scope.errors.other = 'Incorrect password';
           $scope.message = '';
-      
+
         });
       }
 		};
-  
+
 
 
   });
