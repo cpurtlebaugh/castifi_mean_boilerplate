ef20fc8186b804107aa3ef048501a095d5b49f2a
Greg Rock <grock006@gmail.com>
2015-11-05 22:33:27 -0800

    fixed actor photo image defaults

--- client/app/actor/actor.controller.js
+++ client/app/actor/actor.controller.js
@@ -14,8 +14,10 @@ angular.module('castifiApp')
        }
 
          $scope.register = function register(form) {
-          console.log(form)
+          // console.log(form)
+          // console.log(form.$valid)
                $scope.submitted = true;
+               //this might be throwing off the form
                if(form.$valid){
                     if($scope.actor){
                           Actor.update({id: $scope.actor._id }, $scope.actor,
@@ -23,7 +25,7 @@ angular.module('castifiApp')
                                    if($state.is('actor.edit.overview')){ $state.go('actor.edit.photos')};
                                    if($state.is('actor.edit.photos')){ $state.go('actor.edit.physical')};
                                    if($state.is('actor.edit.physical')){ $state.go('actor.edit.wardrobe')};
-                                   if($state.is('actor.edit.wardrobe')){ $state.go('confirmation')};
+                                   if($state.is('actor.edit.wardrobe')){ $state.go('profile')};
                               }),
                               function error(){
                                 $state.go('actor.edit.overview')
