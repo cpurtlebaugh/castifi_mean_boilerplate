6b5062311d8b32e645dd543b15be9c219818beaf
Greg Rock <grock006@gmail.com>
2015-11-10 07:52:14 -0800

    modified gruntfile for admin@castifi.com

--- client/app/actor/actor.controller.js
+++ client/app/actor/actor.controller.js
@@ -9,13 +9,25 @@ angular.module('castifiApp')
        $scope.onlyNumbers = /^[0-9]+$/;
 
        if($scope.actor !== undefined){
-           if($scope.actor.info !== undefined){$scope.actor.info.actingSince  = new Date($scope.actor.info.actingSince)}
-           if($scope.actor.appearance !== undefined){$scope.actor.appearance.pregnantDueDate = new Date($scope.actor.appearance.pregnantDueDate)}
+           if($scope.actor.info !== undefined){
+             var actingSince  = new Date($scope.actor.info.actingSince)
+              console.log(actingSince)
+              console.log(typeof(actingSince))
+              $scope.actor.info.actingSince  = moment(actingSince).format("yyyy-MM-dd")
+              console.log($scope.actor.info.actingSince)
+          }
+           if($scope.actor.appearance !== undefined){
+              var pregnantDueDate = new Date($scope.actor.appearance.pregnantDueDate)
+              console.log(pregnantDueDate)
+              $scope.actor.appearance.pregnantDueDate  = moment(pregnantDueDate).format("yyyy-MM-dd")
+              console.log( $scope.actor.appearance.pregnantDueDate)
+              // console.log( typeof($scope.actor.appearance.pregnantDueDate))
+          }
        }
 
          $scope.register = function register(form) {
-          // console.log(form)
-          // console.log(form.$valid)
+              console.log(form)
+               console.log(form.$valid)
                $scope.submitted = true;
                //this might be throwing off the form
                if(form.$valid){
