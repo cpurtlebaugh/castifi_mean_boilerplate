80bd2be03c484e753926972f73f63d44818b2c05
Greg Rock <grock006@gmail.com>
2015-12-02 21:35:01 -0800

    commented out promises

--- client/components/navbar/panels.js
+++ client/components/navbar/panels.js
@@ -1,14 +1,14 @@
-// 'use strict'
+'use strict'
 
-// angular.module('castifiApp')
-//   .config(function (panelsProvider){
+angular.module('castifiApp')
+  .config(function (panelsProvider){
 
-//     panelsProvider
-//         .add({
-//             id: 'test01',
-//             position: 'left',
-//             size: '80%',
-//             templateUrl: 'components/navbar/navbar-mobile.html',
-//             controller: 'NavbarCtrl'
-//         });
-//   });
+    panelsProvider
+        .add({
+            id: 'test01',
+            position: 'left',
+            size: '80%',
+            templateUrl: 'components/navbar/navbar-mobile.html',
+            controller: 'NavbarCtrl'
+        });
+  });
